2245731da01d6ad1ebf1346d8988b82609134521
Thore Hans-Jörg Weilbier <thore.h.weilbier@campus.tu-berlin.de>
2017-09-14 16:29:55 +0200

    For issue #2. Define basic layout. Create the sructure header, main and footer. Define style for the structure. Create the overall design variables into styles/3v-forhair.scss. Use the package include-media to differ between the mobile and desktop view.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
-    Test
+    <!-- general layout with header, main and footer -->
+    <section class="layout">
+      <header>
+      </header>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+    </section>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -10,13 +21,73 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+  // Import 3rd party styles
+  @import '../node_modules/include-media/dist/include-media';
+
+  // Import own styles
+  @import './style/3v-forhair';
+
+
+  /* Bulma */
+  // Import the variables first.
+  @import '../node_modules/bulma/sass/utilities/initial-variables';
+
+  // Overwrite Bulma variables.
+  $primary: $color-base;
+  $primary-invert: $color-invert;
+
+  // Import Bulma classes afterwards.
+  @import "../node_modules/bulma/bulma";
+
+
+  /* The general layout with header, main and footer */
+  .layout {
+    display: flex;
+    min-height: 100vh;
+    flex-flow: column;
+
+    body {
+      margin: 0;
+      overflow: hidden;
+    }
+
+    header {
+      height: $header-height;
+      overflow: hidden; // Force the height, else the cover page will not work.
+      z-index: 1; // Seems to be necessary, so it is not always to top element.
+      background-color: $color-invert;
+
+      .title {
+        color: black;
+        font-size: 1.5em;
+        font-weight: 500;
+      }
+    }
+
+    main {
+      display: flex;
+      flex-flow: column;
+
+      // Differ the height between mobile and desktop.
+      @include media('<desktop') {
+        height: calc(100vh - #{$footer-height}); // Take the full height without the footer.
+      }
+
+      height: calc(100vh - #{$header-height} - #{$footer-height});
+      overflow: auto; // Necessary if someone switch his mobile device into the horizontal view.
+    }
+
+    footer {
+      // Only display, on non mobile devices.
+      @include media('<desktop') {
+        display: none;
+      }
+
+      height: $footer-height;
+      overflow: hidden; // Force the height, else the cover page will not work.
+      background-color: $primary-invert;
+    }
+  }
 </style>
