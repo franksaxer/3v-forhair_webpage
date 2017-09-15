7cb09ef0add0f3b22bca95f738ff04e8f3a4cf70
Thore Hans-Jörg Weilbier <thore.h.weilbier@campus.tu-berlin.de>
2017-09-15 10:09:44 +0200

    For issue #5. Differ between mobile and desktop view. Put the banner to the bottom on mobile devices. Add a button to close the banner. Only available in the mobile view.

--- client/src/components/AdvertismentBanner.vue
+++ client/src/components/AdvertismentBanner.vue
@@ -1,11 +1,27 @@
 <template>
-  <div id="advertisement"></div>
+  <div  id="advertisement"
+        :class="{'closed' : closed}">
+
+    <button class="ghost-button"
+            @click="closed = true">
+
+      <i class="fa fa-times-circle"
+         aria-hidden="true">
+      </i>
+    </button>
+  </div>
 </template>
 
 
 <script>
   export default {
-    name: 'advertisement-banner'
+    name: 'advertisement-banner',
+
+    data: function () {
+      return {
+        closed: false
+      }
+    }
   }
 </script>
 
@@ -19,18 +35,61 @@
   @import '../style/3v-forhair';
 
 
+  // Define some variables.
+  $banner-height: 300px;
+
+
   #advertisement {
+    /* Shapes */
     left: calc(100% - #{$advertisement-width});
     right: 0;
+    // Inherit the top and bottom properties from the upper component.
 
+
+    /* Background */
     background: url('../assets/advertisement/aveda-example.png');
     background-position: center left;
     background-repeat:  no-repeat;
     background-size: cover;
 
+
+    /* Mobile Properties */
     @include media('<desktop') {
-      // Hide the advertisement on mobile devices to safe space.
-      display: none;
+      /* Shapes */
+      left: 0;
+      top: calc(100vh - #{$banner-height});
+      right: 0;
+      bottom: 0;
+
+      &.closed {
+        display: none;
+      }
     }
+
+
+    /* Close Button */
+    button {
+      /* Variables */
+      $margin: 5px;
+
+
+      /* Position and Shape */
+      position: absolute;
+      top: $margin;
+      right: $margin;
+
+
+      /* Effects */
+      &:hover {
+        color: gray;
+      }
+
+
+      @include media('>=desktop') {
+        // Should be only removable on mobile devices.
+        display: none;
+      }
+    }
+
   }
 </style>
