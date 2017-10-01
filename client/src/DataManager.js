e1b7e4cf37b97b632f74a55a2ab9cc69badea2c6
Thore Hans-Jörg Weilbier <thore.h.weilbier@campus.tu-berlin.de>
2017-10-01 17:50:01 +0200

    For issue #13. Introduce the DataManager. Add a new component to read in JSON data files. Uses the DataStoreEnum to describe which file is required. Convert the ContactData.js to a JSON file. Load the contact data in the App.vue with the DataManager.

---
+++ client/src/DataManager.js
@@ -0,0 +1,49 @@
+/* Import Enums */
+import {DataStoreEnum, checkEntry} from './enums/DataStoreEnum'
+
+/* Import JSON Files */
+import JsonContact from './data/ContactData.json'
+
+/**
+ * Local store where to put in already loaded data objects for faster future access.
+ */
+const DATA_STORE = {}
+
+/**
+ * Function to load a data object.
+ * Checks if the data object is known or not. In case it is not, NULL will be returned.
+ * Load the object from the internal store, if it has been already loaded earlier.
+ * @param   data
+ *          Based on the DataStoreEnum, used to define which object should be load.
+ * @return  The required data object.
+ */
+const loadDataObject = function (data) {
+  // Check if it is a valid data store object.
+  if (!checkEntry(data)) {
+    return null
+  }
+
+  // Load JSON if not stored internally from an earlier call.
+  if (!DATA_STORE.data) {
+    let json = null
+
+    switch (data) {
+      case DataStoreEnum.contact:
+        json = JsonContact
+        break
+
+      default:
+        json = null
+    }
+
+    DATA_STORE.data = json
+  }
+
+  // Return the data object.
+  return DATA_STORE.data
+}
+
+// Define what should be exported.
+export default {
+  loadData: loadDataObject
+}
--- client/src/data/ContactData.js
+++
@@ -1,4 +0,0 @@
-export default {
-  phoneNumber: '030 30104598',
-  mailAddress: 'test@beispiel.de'
-}
