2f24c2653684e4b52cb7d94adb3a72910628344c
Thore Hans-Jörg Weilbier <thore.h.weilbier@campus.tu-berlin.de>
2017-10-01 20:27:30 +0200

    For issue #7. Add new enumeration for URLs.

---
+++ server/src/enums/UrlEnum.js
@@ -0,0 +1,14 @@
+/**
+ * The enumeration object with the keys.
+ */
+const UrlEnum = {
+  admin: 'jt3yz8MzMKafBy5Q'
+}
+
+// Define what should be exported.
+export {UrlEnum}
+
+// Per default only export the enumeration.
+export default DataStoreEnum
+
+
