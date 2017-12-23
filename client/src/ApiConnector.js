9a64be3ced368ab0c7f701e8c055ca78dfd0e01a
Thore Hans-Jörg Weilbier <thore.h.weilbier@campus.tu-berlin.de>
2017-12-23 11:24:04 +0100

    Add the upload function to the client.
    Extend the ApiConnector with the upload file function.
    Create a form data object, where it place the media path and file.

--- client/src/ApiConnector.js
+++ client/src/ApiConnector.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 
-const authenticate = async function (password) {
+const login = async function (password) {
   const data = {password: password}
 
   try {
     return await Vue.http.post('https://localhost:8081/api/authentication/login', data).then(response => {
       if (response.status === 200) {
         return response.body
+      } else if (response.status === 401) {
+        throw new Error('Wrong password')
       } else {
         console.log('Error while connect to server. Response: ' + response.status + ' - ' + response.body)
         throw new Error('Something went wrong while try to connect the server.')
@@ -14,14 +16,51 @@ const authenticate = async function (password) {
     }, error => {
       console.log('Error while connect to server. Response: ')
       console.log(JSON.stringify(error))
-      throw new Error('Could not connect to the server.')
+      throw new Error(error.bodyText)
     })
   } catch (err) {
+    console.log(err)
+    // Just forward the error.
+    throw err
+  }
+}
+
+const logout = async function (sessionKey) {
+  const data = {sessionKey: sessionKey}
+
+  try {
+    await Vue.http.post('https://localhost:8081/api/authentication/logout', data).then(response => {
+      // Nothing to do.
+    }, error => {
+      throw new Error(error.bodyText)
+    })
+  } catch (err) {
+    console.log(err)
+    // Just forward the error.
+    throw err
+  }
+}
+
+const upload = async function (path, file) {
+  const data = new FormData()
+  data.append('mediaPath', path)
+  data.append('mediaFile', file)
+
+  try {
+    await Vue.http.put('https://localhost:8081/api/editor/upload', data).then(response => {
+      // Nothing to do.
+    }, error => {
+      throw new Error(error.bodyText)
+    })
+  } catch (err) {
+    console.log(err)
     // Just forward the error.
     throw err
   }
 }
 
 export default {
-  authenticate: authenticate
+  login: login,
+  logout: logout,
+  upload: upload
 }
