4832c8649cfe0cbd981374b29f114e915690e5d1
weilbith <thore@weilbier.net>
2018-01-01 17:01:05 +0100

    Implement DeviceDetector for #19.
    Define enumeration of device types in relation to the _include media_
    package.
    Implement the DeviceConnector with `isMobile()` and `isDesktop()`.
    Define the breakpoints object in relation to the _include media_
    package.

---
+++ client/src/enums/DeviceTypeEnum.js
@@ -0,0 +1,9 @@
+/*
+ * Enumeration that includes all device types.
+ * These types are according to the include media package.
+ */
+export default {
+  phone: 'f7a42fe7211f98ac7a60a285ac3a9e87',
+  tablet: 'c4e0ba5422383082417c5f96ab121575',
+  desktop: 'b2c79ad7dcf03ba266dc0885e1266675'
+}
