fceb8d66e678ed02e29e3467f7baa79526533c95
weilbith <thore@weilbier.net>
2018-03-15 20:22:00 +0100

    Rework the label store by prepare for the plugin.

---
+++ client/src/enums/LabelKeys.js
@@ -0,0 +1,16 @@
+/*
+ * Conventions:
+ *  - The keys are constant, so write them uppercase.
+ *  - Use underline characters to separate words/segments.
+ *  - Start with a section name (e.g. AUTH) and keep sections together.
+ *  - If possible define the element it is bind to like a button (BTN).
+ *  - Longer messages to the user, which describe something are shorten to MSG and then the topic.
+ *  - Always try to get a classification which can be reused for similar labels.
+ *  - Use conventional abbreviations, cause you have to write them everywhere this label is used.
+ */
+
+export const AUTH_MSG_DESCRIPTION = 'b9ee72e398f01cb5b54331303a43d457'
+export const AUTH_MSG_INVALID_PWD = 'dc3e9b51c2cefc39c4989ce451ae2d40'
+export const AUTH_INPUT_PH_PWD = '199cdbc390ac7c4cac64635788dd9b81'
+export const AUTH_BTN_LOGIN = 'e59b07884d62d64e5d7d1c175c52b13d'
+export const AUTH_BTN_LOGOUT = '66aac14226219aeb08fe718c82568bc6'
---
+++ client/src/enums/Languages.js
@@ -0,0 +1,2 @@
+export const EN = 'e3e64deb07c5945e1bbf4e9f2f006697'
+export const DE = '09c438e63455e3e1b3deabe65fdbc087'
