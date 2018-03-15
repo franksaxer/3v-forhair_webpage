27e53a48de4e9014ae949a60c4c7f40f7d8bbfaf
weilbith <thore@weilbier.net>
2018-03-15 20:29:31 +0100

    Introduce the mulilang-store plugin.

--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,23 @@ import Vue from 'vue'
 import Resource from 'vue-resource'
 import VDragged from 'v-dragged'
 import DeviceDetector from 'vue-device-detector'
+import MultilangStore from 'vue-multilang-store'
+
+// Other stuff
+import * as langs from './enums/Languages'
+import * as keys from './enums/LabelKeys'
+import labels from './data/Labels'
 
 // Register the plugins.
 Vue.use(Resource)
 Vue.use(VDragged)
 Vue.use(DeviceDetector)
+Vue.use(MultilangStore, {
+  languageList: langs,
+  defaultLanguage: langs.EN,
+  labels: labels,
+  labelKeys: keys
+})
 
 // Import global components
 import App from './App'
