b249072a1e1430bef127efbcfe561f4c413285fd
weilbith <thore@weilbier.net>
2018-03-16 22:08:46 +0100

    Provide the editable slot component and function.
    Use a global registered component to nest editable content into it.
    Provide a function, which can be called to en- or disable the content to
    be shown.

--- client/src/plugins/EditableView.js
+++ client/src/plugins/EditableView.js
@@ -1,25 +1,34 @@
 // The plugin itself.
 const EditableView = {
   install: function (Vue, options) {
-    /* Mixin */
+    // Define a communication bus for the editable view.
+    window.editableViewBus = new Vue()
 
-    Vue.mixin({
-      computed: {
-        editableViewEnabled () {
-          return false
+    /**
+     * Function that allows to en- or disable the editable view.
+     */
+    Vue.prototype.$setEditable = function (enable = true) {
+      window.editableViewBus.$emit('setEditable', enable)
+    }
+
+    /**
+     * A wrapper component used to hide editable components within.
+     * It provides a slot, which can be filled with anything.
+     * The whole content will be not shown, if the editable view is not enabled.
+     */
+    Vue.component('editable', {
+      // Simple template with the if statement and the slot.
+      template: '<div v-if="enabled"><slot></slot></div>',
+
+      data: function () {
+        return {
+          enabled: false // Per default ediable content should not been visible.
         }
-      }
-    })
+      },
 
-    /* Directory */
-
-    Vue.directive('editable', {
-      bind (el, binding, vnode) {
-        // for (let i in vnode.data) {
-          // console.log(i + ' ' + vnode.data[i])
-        // }
-        vnode.data.class = {'test': true}
-        console.log(vnode.data.class)
+      // Catch the event to show the slots content.
+      created () {
+        window.editableViewBus.$on('setEditable', enable => { this.enabled = enable })
       }
     })
   }
