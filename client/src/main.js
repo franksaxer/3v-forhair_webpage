1651f403b89d82f7c1a2e639bf5ff4b3aaa26f73
weilbith <thore@weilbier.net>
2018-04-23 11:37:18 +0200

    Adjust the youtube video depending on the window.
    Switch the vue plugin for that.

--- client/src/main.js
+++ client/src/main.js
@@ -37,7 +37,7 @@ Vue.use(MultilangStore, {
 // Import global components
 import App from './App'
 import VueToggle from 'vue-toggle'
-import VueYoutube from 'vue-youtube'
+import VueYoutube from 'vue-youtube-embed'
 import VeeValidate from 'vee-validate'
 
 VeeValidate.Validator.extend('passphrase', {
