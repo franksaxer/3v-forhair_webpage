d13e60e87d5d742a4c511381bec54dec610934e5
jonathan <jonathan-zimmermann94@gmx.de>
2020-06-13 22:42:40 +0200

    modifed texts

--- client/src/components/AdvertisementBanner/Message.vue
+++ client/src/components/AdvertisementBanner/Message.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="is-size-6 has-text-weight-medium ">
-    3v-forhair ist <a href="https://www.aveda.de/salon/3v-forhair" target="_blank">offizieller Partner</a> von <i>AVEDA</I>. Kosmetikprodulkte mit natürlichen Minerailien aus einer verantwortungsvoller Produktion.
+    3v-forhair ist <a href="https://www.aveda.de/salon/3v-forhair" target="_blank">offizieller Partner</a> von AVEDA.
   </p>
 </template>
 
