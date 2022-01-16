c0660e1c716556df58757b6335d51398a8399365
j0n1han <jonathan-zimmermann94@gmx.de>
2022-01-16 11:51:59 +0100

    added employee meli

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -1,7 +1,7 @@
-var merge = require('webpack-merge')
+const merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://localhost:8081"'
+  API_URL: '"https://localhost:8081"',
 })
